2febf1fcc7ff2059d8a6e43dc75e66ee08d7ec04
lennelov <lennelov@kth.se>
2020-01-25 22:38:44 +0100

    Split files

--- lab1/4_3.py
+++ lab1/4_3.py
@@ -3,7 +3,12 @@ import matplotlib.pyplot as plt
 import torch
 import sys
 
+from fully_connected_net import FullyConnectedNet
+
 plt.style.use('ggplot')
+
+
+# ================================ DATA HANDLING ===============================
 
 def mackey_glass_time_series(n_steps, step_size=1, x_0=1.5, beta=0.2, gamma=0.1, n=10, tau=25, plot=False):
     x = np.zeros((n_steps))
@@ -19,20 +24,25 @@ def mackey_glass_time_series(n_steps, step_size=1, x_0=1.5, beta=0.2, gamma=0.1,
 
     return x
 
-def generate_data(n_samples, offset=300):
-    series = mackey_glass_time_series(n_samples+offset+5)
+
+def generate_data(n_samples, offset=300, plot=False):
+    series = mackey_glass_time_series(n_samples+offset+5, plot=plot)
     patterns = []
     targets = []
     for t in range(offset, n_samples+offset):
         patterns.append([series[t-20], series[t-15], series[t-10], series[t-5], series[t]])
         targets.append(series[t+5])
 
-    return np.array(patterns), np.array(targets)
+    data = np.column_stack((np.array(patterns), np.array(targets)))
+    # data = (data - np.mean(data))/np.std(data)
+    return data[:, :-1], data[:, -1]
+
 
 def extend_with_bias(patterns):
     n_patterns = patterns.shape[0]
     ones = torch.ones((n_patterns, 1))
     return torch.cat((patterns, ones), 1)
+
 
 def split_data(patterns, targets, validation_fraction=0.15, test_fraction=0.25):
     n_samples = len(patterns)
@@ -50,136 +60,112 @@ def split_data(patterns, targets, validation_fraction=0.15, test_fraction=0.25):
     return train_patterns, train_targets, validation_patterns, validation_targets, test_patterns, test_targets
 
 
-class FullyConnectedNet:
-    def __init__(self, network_dims, learning_rate=1e-6, convergence_threshold=1e-9,
-                       max_iter=100000, reg_factor=0.0005):
-        # Set up network dimensions
-        self.network_dims = network_dims
-        self.dim_in = network_dims[0]
-        self.dim_hidden = network_dims[1:-1]
-        self.dim_out = network_dims[-1]
+# ================================ PLOTTING FUNCTIONS ===============================
 
-        # Set up weights
-        self.weights_in = torch.randn(self.dim_in, self.dim_hidden[0], requires_grad=True)
-        self.weights_hidden = []
-        for i in range(len(self.dim_hidden)-1):
-            self.weights_hidden.append(
-                torch.randn(self.dim_hidden[i], self.dim_hidden[i+1], requires_grad=True))
-        print(len(self.weights_hidden))
-        self.weights_out = torch.randn(self.dim_hidden[-1], self.dim_out, requires_grad=True)
-        self.weights_all = [self.weights_in, *self.weights_hidden, self.weights_out]
+def plot_weights_histogram(weights_list):
+    for weights in weights_list:
+        print(weights.shape)
+    flat_weights = [w.detach().numpy().flatten() for w in weights_list]
+    all_weights = np.concatenate(flat_weights)
 
-        # Set up learning hyperparameters
-        self.learning_rate = learning_rate
-        self.convergence_threshold = convergence_threshold
-        self.max_iter = max_iter
-
-        # Set up bookkeeping
-        self.validation_loss_record = None
-
-    def forward(self, input):
-        # Input layer
-        x = torch.sigmoid(torch.mm(input, self.weights_in))
-        # Hidden layers
-        for weights in self.weights_hidden:
-            x = torch.sigmoid(torch.mm(x, weights))
-        # Output layer
-        output = torch.mm(x, self.weights_out)
-
-        return output
-
-    def loss(self, predictions, targets):
-        return (predictions - targets).pow(2).sum()
-
-    def l1_regularizer(self, weights_set):
-        loss = 0
-        for weights in weights_set:
-            loss += weights.abs().sum()
-        return loss
-
-    def l2_regularizer(self, weights_set):
-        loss = 0
-        for weights in weights_set:
-            loss += weights.pow(2).sum()
-        return loss
-
-    def reg_loss(self, weights_set, mode="l2"):
-        if mode == "l2":
-            return l2_regularizer(weights_set)
-        elif mode == "l1":
-            return l1_regularizer(weights_set)
-
-    def fit(self, train_patterns, train_targets, validation_patterns, validation_targets):
-        # Add column of 1's to handle bias
-
-        validation_loss = np.infty
-        delta_validation_loss = np.infty
-        iter = 0
-        self.validation_loss_record = []
-
-        while delta_validation_loss > self.convergence_threshold and iter < self.max_iter:
-            old_validation_loss = validation_loss
-
-            train_predictions = self.forward(train_patterns)
-            train_loss = (self.loss(train_predictions, train_targets)
-                          + self.reg_factor * self.reg_loss(self.weights_all))
-            train_loss.backward()
-
-            with torch.no_grad():
-                for weights in self.weights_all:
-                    weights -= learning_rate*weights.grad
-                    weights.grad.zero_()
-
-                validation_predictions = self.forward(validation_patterns)
-                validation_loss = self.loss(validation_predictions, validation_targets)
-                self.validation_loss_record.append(validation_loss/len(validation_targets))
-                delta_validation_loss = abs(old_validation_loss - validation_loss)
-
-            # Progress bar
-            if iter % int(max_iter/10) == 0:
-                print("Training progress: {}".format(iter/max_iter))
-                print("Iteration number {}. Average validation loss per sample: {:.2f}".format(
-                    iter, validation_loss/len(validation_targets)))
-
-            iter += 1
+    fig, ax = plt.subplots(1, 1, sharey=True, tight_layout=True)
+    ax.hist(all_weights, bins=20)
+    plt.show()
 
 
 
-# Hyperparameters
-hidden_layer_dims = [8]
-learning_rate = 1e-10     #1e-6 seems to be the largest usable learning rate
-convergence_threshold = 1e-15
-max_iter = 20000
-n_samples = 1200
 
-# Generate and process data
-patterns, targets = generate_data(n_samples)
-train_patterns, train_targets, validation_patterns, validation_targets,  test_patterns, test_targets = split_data(
-    patterns, targets, validation_fraction=200/1200, test_fraction=200/1200)
 
-# Set up network
-dim_in = train_patterns.shape[1]
-dim_out = 1
-layer_dims = [dim_in, *hidden_layer_dims, dim_out]
-net = FullyConnectedNet(layer_dims,
-                        learning_rate=learning_rate,
-                        convergence_threshold=convergence_threshold,
-                        max_iter=max_iter)
 
-# Train networks
-net.fit(train_patterns, train_targets, validation_patterns, validation_targets)
 
-# Plot convergence of validation loss
-plt.plot(net.validation_loss_record[20:])
-plt.show()
+# ================================ BENCHMARKING AND TESTING ===============================
 
-# Test
-test_predictions = net.forward(test_patterns)
-test_loss = net.loss(test_predictions, test_targets)
-print("Average test loss per sample: {:.2f}".format(test_loss/len(test_predictions)))
-plt.plot(test_predictions.detach().numpy(), label='Prediction')
-plt.plot(test_targets.detach().numpy(), label='Target')
-plt.legend()
-plt.show()
+def average_loss(n_trials, hidden_layer_dims, learning_rate, convergence_threshold, max_iter, reg_factor):
 
-print("end")
+    n_samples = 1200
+
+    # Generate and process data
+    patterns, targets = generate_data(n_samples, plot=True)
+    train_patterns, train_targets, validation_patterns, validation_targets,  test_patterns, test_targets = split_data(
+        patterns, targets, validation_fraction=200/1200, test_fraction=200/1200)
+
+
+    # Set up network
+    dim_in = train_patterns.shape[1]
+    print(train_patterns[0])
+    dim_out = 1
+    layer_dims = [dim_in, *hidden_layer_dims, dim_out]
+
+    losses = []
+    for trial in range(n_trials):
+        net = FullyConnectedNet(layer_dims,
+                                learning_rate=learning_rate,
+                                convergence_threshold=convergence_threshold,
+                                max_iter=max_iter,
+                                reg_factor=reg_factor)
+
+        net.fit(train_patterns, train_targets, validation_patterns, validation_targets)
+        # net.fit_with_torch_optimizer(train_patterns, train_targets, validation_patterns, validation_targets)
+
+        test_predictions = net.forward(test_patterns)
+        test_loss = net.loss(test_predictions, test_targets)
+        losses.append(test_loss.item() / n_trials)
+
+    return np.mean(losses), np.std(losses)
+
+def test_average():
+
+    hidden_layer_dims = [6]
+    learning_rate = 1e-8     #1e-6 seems to be the largest usable learning rate
+    convergence_threshold = 1e-13
+    max_iter = 100
+    reg_factor = 0.0005
+    print(average_loss(100, hidden_layer_dims, learning_rate, convergence_threshold, max_iter, reg_factor))
+
+def main():
+    # Hyperparameters
+    hidden_layer_dims = [6]
+    learning_rate = 1e-10     #1e-6 seems to be the largest usable learning rate
+    convergence_threshold = 1e-13
+    max_iter = 100
+    reg_factor = 0.0005
+    n_samples = 1200
+
+    # Generate and process data
+    patterns, targets = generate_data(n_samples, plot=True)
+    train_patterns, train_targets, validation_patterns, validation_targets,  test_patterns, test_targets = split_data(
+        patterns, targets, validation_fraction=200/1200, test_fraction=200/1200)
+
+    # Set up network
+    dim_in = train_patterns.shape[1]
+    print(train_patterns[0])
+    dim_out = 1
+    layer_dims = [dim_in, *hidden_layer_dims, dim_out]
+    net = FullyConnectedNet(layer_dims,
+                            learning_rate=learning_rate,
+                            convergence_threshold=convergence_threshold,
+                            max_iter=max_iter,
+                            reg_factor=reg_factor)
+
+    # Train networks
+    net.fit(train_patterns, train_targets, validation_patterns, validation_targets)
+    # net.fit_with_torch_optimizer(train_patterns, train_targets, validation_patterns, validation_targets)
+
+    plot_weights_histogram(net.weights_all)
+
+    # Plot convergence of validation loss
+    plt.plot(net.validation_loss_record[:])
+    plt.show()
+
+    # Test
+    test_predictions = net.forward(test_patterns)
+    test_loss = net.loss(test_predictions, test_targets)
+    print("Average test loss per sample: {:.2f}".format(test_loss/len(test_predictions)))
+    plt.plot(test_predictions.detach().numpy(), label='Prediction')
+    plt.plot(test_targets.detach().numpy(), label='Target')
+    plt.legend()
+    plt.show()
+
+    print("end")
+
+main()
---
+++ lab1/fully_connected_net.py
@@ -0,0 +1,118 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import sys
+
+class FullyConnectedNet:
+    def __init__(self, network_dims, learning_rate=1e-6, convergence_threshold=1e-9,
+                       max_iter=100000, reg_factor=0.0005):
+        # Set up network dimensions
+        self.network_dims = network_dims
+        self.dim_in = network_dims[0]
+        self.dim_hidden = network_dims[1:-1]
+        self.dim_out = network_dims[-1]
+
+        # Set up weights
+        self.weights_in = torch.randn(self.dim_in, self.dim_hidden[0], requires_grad=True)
+        self.weights_hidden = []
+        for i in range(len(self.dim_hidden)-1):
+            self.weights_hidden.append(
+                torch.randn(self.dim_hidden[i], self.dim_hidden[i+1], requires_grad=True))
+        print(len(self.weights_hidden))
+        self.weights_out = torch.randn(self.dim_hidden[-1], self.dim_out, requires_grad=True)
+        self.weights_all = [self.weights_in, *self.weights_hidden, self.weights_out]
+
+        # Set up learning hyperparameters
+        self.learning_rate = learning_rate
+        self.convergence_threshold = convergence_threshold
+        self.max_iter = max_iter
+        self.reg_factor = reg_factor
+
+        # Set up bookkeeping
+        self.validation_loss_record = None
+
+    def forward(self, input):
+        # Input layer
+        x = torch.tanh(torch.mm(input, self.weights_in))
+        # Hidden layers
+        for weights in self.weights_hidden:
+            x = torch.tanh(torch.mm(x, weights))
+        # Output layer
+        output = torch.mm(x, self.weights_out)
+
+        return output
+
+    def loss(self, predictions, targets):
+        return (predictions - targets).pow(2).sum()
+
+    def l1_regularizer(self, weights_set):
+        loss = 0
+        for weights in weights_set:
+            loss += weights.abs().sum()
+        return loss
+
+    def l2_regularizer(self, weights_set):
+        loss = 0
+        for weights in weights_set:
+            loss += weights.pow(2).sum()
+        return loss
+
+    def reg_loss(self, weights_set, mode=None):
+        if not mode:
+            return 0
+        elif mode == "l1":
+            return self.l1_regularizer(weights_set)
+        elif mode == "l2":
+            return self.l2_regularizer(weights_set)
+
+    def fit(self, train_patterns, train_targets, validation_patterns, validation_targets):
+        validation_loss = np.infty
+        delta_validation_loss = np.infty
+        iter = 0
+        self.validation_loss_record = []
+
+        # while delta_validation_loss > self.convergence_threshold and iter < self.max_iter:
+        while iter < self.max_iter:
+            old_validation_loss = validation_loss
+
+            train_predictions = self.forward(train_patterns)
+            train_loss = (self.loss(train_predictions, train_targets)
+                          + self.reg_factor * self.reg_loss(self.weights_all, mode=None))
+            train_loss.backward()
+
+            with torch.no_grad():
+                for weights in self.weights_all:
+                    weights -= self.learning_rate*weights.grad
+                for weights in self.weights_all:
+                    weights.grad.zero_()
+
+                validation_predictions = self.forward(validation_patterns)
+                validation_loss = self.loss(validation_predictions, validation_targets).item()
+                self.validation_loss_record.append(validation_loss/len(validation_targets))
+                delta_validation_loss = abs(old_validation_loss - validation_loss)
+
+            # Progress bar
+            if iter % int(self.max_iter/10) == 0:
+                print("Training progress: {}".format(iter/self.max_iter))
+                print("Iteration number {}. Average validation loss per sample: {:.2f}".format(
+                    iter, validation_loss/len(validation_targets)))
+
+            iter += 1
+
+    def fit_with_torch_optimizer(self, train_patterns, train_targets, validation_patterns,
+                                 validation_targets, learning_rate=1e-4):
+        optimizer = torch.optim.Adam(self.weights_all, lr=learning_rate)
+        self.validation_loss_record = []
+        for t in range(1000):
+            train_predictions = self.forward(train_patterns)
+            loss = self.loss(train_predictions, train_targets)
+            if t%100 == 0:
+                print(t, loss.item())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            with torch.no_grad():
+                validation_predictions = self.forward(validation_patterns)
+                validation_loss = float(self.loss(validation_predictions, validation_targets).item())
+                self.validation_loss_record.append(validation_loss/len(validation_targets))
