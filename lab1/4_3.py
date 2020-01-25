8aa1b4da907492aff8ba81975b2f5d15a175a116
lennelov <lennelov@kth.se>
2020-01-25 11:52:53 +0100

    Make object oriented

--- lab1/4_3.py
+++ lab1/4_3.py
@@ -39,67 +39,109 @@ def split_data(patterns, targets, validation_fraction=0.15, test_fraction=0.25):
     return train_patterns, train_targets, validation_patterns, validation_targets, test_patterns, test_targets
 
 
+class FullyConnectedNet:
+    def __init__(self, network_dims, learning_rate=1e-6, convergence_threshold=1e-9, max_iter=100000):
+        # Set up network dimensions
+        self.network_dims = network_dims
+        self.dim_in = network_dims[0]
+        self.dim_hidden = network_dims[1:-1]
+        self.dim_out = network_dims[-1]
+
+        # Set up weights
+        self.weights_in = torch.randn(self.dim_in, self.dim_hidden[0], requires_grad=True)
+        self.weights_hidden = []
+        for i in range(len(self.dim_hidden)-1):
+            self.weights_hidden.append(
+                torch.randn(self.dim_hidden[i], self.dim_hidden[i+1], requires_grad=True))
+        self.weights_out = torch.randn(self.dim_hidden[-1], self.dim_out, requires_grad=True)
+        self.weights_all = [self.weights_in, *self.weights_hidden, self.weights_out]
+
+        # Set up learning hyperparameters
+        self.learning_rate = learning_rate
+        self.convergence_threshold = convergence_threshold
+        self.max_iter = max_iter
+
+        # Set up bookkeeping
+        self.validation_loss_record = None
+
+    def forward(self, input):
+        # Input layer
+        x = torch.sigmoid(torch.mm(input, self.weights_in))
+        # Hidden layers
+        for weights in self.weights_hidden:
+            x = torch.sigmoid(torch.mm(x, self.weights))
+        # Output layer
+        output = torch.mm(x, self.weights_out)
+
+        return output
+
+    def loss(self, predictions, targets):
+        return (predictions - targets).pow(2).sum()
+
+    def fit(self, train_patterns, train_targets, validation_patterns, validation_targets):
+        validation_loss = np.infty
+        delta_validation_loss = np.infty
+        iter = 0
+        self.validation_loss_record = []
+
+        while delta_validation_loss > self.convergence_threshold and iter < self.max_iter:
+            old_validation_loss = validation_loss
+
+            train_predictions = self.forward(train_patterns)
+            train_loss = self.loss(train_predictions, train_targets)
+            train_loss.backward()
+
+            with torch.no_grad():
+                for weights in self.weights_all:
+                    weights -= learning_rate*weights.grad
+                    weights.grad.zero_()
+
+                validation_predictions = self.forward(validation_patterns)
+                validation_loss = self.loss(validation_predictions, validation_targets)
+                self.validation_loss_record.append(validation_loss)
+                delta_validation_loss = abs(old_validation_loss - validation_loss)
+
+            # Progress bar
+            if iter % int(max_iter/10) == 0:
+                print(iter/max_iter)
+                print(iter, train_loss.item())
+
+            iter += 1
 
 
-device = torch.device('cpu')
 
+# Hyperparameters
+hidden_layer_dims = [5]
+learning_rate = 1e-8
+convergence_threshold = 1e-12
 max_iter = 10000
 n_samples = 1200
-dim_hidden_1 = 5
 
+# Generate and process data
 patterns, targets = generate_data(n_samples)
 train_patterns, train_targets, validation_patterns, validation_targets,  test_patterns, test_targets = split_data(
-    patterns, targets, validation_fraction=400/1200, test_fraction=200/1200)
+    patterns, targets, validation_fraction=200/1200, test_fraction=200/1200)
 
+# Set up network
 dim_in = train_patterns.shape[1]
 dim_out = 1
+layer_dims = [dim_in, *hidden_layer_dims, dim_out]
+net = FullyConnectedNet(layer_dims,
+                        learning_rate=learning_rate,
+                        convergence_threshold=convergence_threshold,
+                        max_iter=max_iter)
 
-weights_1 = torch.randn(dim_in, dim_hidden_1, requires_grad=True)
-weights_2 = torch.randn(dim_hidden_1, dim_out, requires_grad=True)
+# Train networks
+net.fit(train_patterns, train_targets, validation_patterns, validation_targets)
 
-learning_rate = 1e-7
-convergence_threshold = 1e-8
-train_loss = np.infty
-delta_train_loss = np.infty
-
-iter = 0
-validation_loss_record = []
-while delta_train_loss > convergence_threshold and iter <= max_iter:
-    old_train_loss = train_loss
-
-    train_predictions = torch.nn.functional.sigmoid(train_patterns.mm(weights_1)).mm(weights_2)
-    train_loss = (train_predictions - train_targets).pow(2).sum()
-    train_loss.backward()
-
-    with torch.no_grad():
-        weights_1 -= learning_rate * weights_1.grad
-        weights_2 -= learning_rate * weights_2.grad
-        weights_1.grad.zero_()
-        weights_2.grad.zero_()
-
-    if iter % int(max_iter/10) == 0:
-        print(iter/max_iter)
-        print(iter, train_loss.item())
-
-    delta_train_loss = abs(old_train_loss - train_loss)
-    iter += 1
-    with torch.no_grad():
-        validation_predictions = torch.nn.functional.sigmoid(validation_patterns.mm(weights_1)).mm(weights_2)
-        validation_loss = (validation_predictions - validation_targets).pow(2).sum().item()
-        validation_loss_record.append(validation_loss)
-
-print(iter)
-print(len(validation_loss_record))
-plt.plot(validation_loss_record[20:])
+# Plot convergence of validation loss
+plt.plot(net.validation_loss_record[20:])
 plt.show()
 
-
-
-test_predictions = torch.nn.functional.relu(test_patterns.mm(weights_1)).mm(weights_2)
-test_loss = (test_predictions - test_targets).pow(2).sum()
-print("Test loss: {}".format(test_loss))
-
-
+# Test
+test_predictions = net.forward(test_patterns)
+test_loss = net.loss(test_predictions, test_targets)
+print("Test loss: {}".format(test_loss/len(test_predictions)))
 plt.plot(test_predictions.detach().numpy(), label='Prediction')
 plt.plot(test_targets.detach().numpy(), label='Target')
 plt.legend()
