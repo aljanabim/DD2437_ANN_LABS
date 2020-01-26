fa9446e15e1be44d5b58b42b7841f79f00a790ba
Mustafa Al-Janabi <ma970101@yahoo.se>
2020-01-26 19:03:31 +0100

    S

--- lab1/3_2_fullbias.py
+++ lab1/3_2_fullbias.py
@@ -5,6 +5,8 @@ from matplotlib import pyplot as plt
 from generate_data import generate_data
 plt.style.use('ggplot')
 
+np.random.seed(17)
+
 
 class NeuralNetwork():
     def __init__(self, method='batch', learning_rate=0.001, n_inputs=2, n_hidden=5, n_outputs=1):
@@ -19,22 +21,19 @@ class NeuralNetwork():
         self.x = np.zeros(self.n_inputs)
         self.h = np.zeros(self.n_hidden)
         self.y = np.zeros(self.n_outputs)
-        cov_v = np.array([[1, 0],
-                          [0, 1]])
-        mean_v = np.array([0, 0])
+
+        cov_v = np.eye(self.n_inputs+1)
+        mean_v = np.zeros(self.n_inputs+1)
         self.v = np.random.multivariate_normal(
             mean_v, cov_v, self.n_hidden)
-        cov_w = np.eye(self.n_hidden)
-        mean_w = np.zeros(self.n_hidden)
+
+        cov_w = np.eye(self.n_hidden+1)
+        mean_w = np.zeros(self.n_hidden+1)
         self.w = np.random.multivariate_normal(
             mean_w, cov_w, self.n_outputs)
 
     def _add_bias(self):
-        # To data
-        self.data = np.column_stack((self.data, np.ones(self.data.shape[0])))
-        # To hidden layers weights
-        self.v = np.column_stack((self.v, np.ones(self.n_hidden)))
-        # print(self.v)
+        self.data = np.column_stack((self.data, np.ones(data.shape[0])))
 
     def _activation(self, x):
         return np.tanh(x)
@@ -44,35 +43,28 @@ class NeuralNetwork():
 
     def _forward_pass(self):
         self.h = self._activation(np.dot(self.v, self.data.T))
+        self.h = np.row_stack((self.h, np.ones(self.n_data)))  # add bias
         self.y = self._activation(np.dot(self.w, self.h))
 
     def _backward_pass(self, labels):
         # calc delta_k
         y_in = np.dot(self.w, self.h)
         self.delta_k = (labels - self.y) * self._activation_deriv(y_in)
+
         # calc delta_j
         h_in = np.dot(self.v, self.data.T)
-        self.delta_j = np.zeros((self.n_hidden, self.n_data))
-
+        h_in = np.row_stack((h_in, np.ones(self.n_data)))  # add bias
         delta_k_w = np.dot(np.array(self.delta_k).T, self.w)
         h_in_deriv = self._activation_deriv((h_in))
         self.delta_j = np.multiply(delta_k_w.T, h_in_deriv)
-
-        # Stupid way using for loops
-        # for j in range(self.n_data):
-        #     h_in_deriv = self._activation_deriv((h_in[:, j]))
-        #     self.delta_j[:, j] = np.multiply(
-        #         np.dot(self.delta_k[:, j], self.w), h_in_deriv)
+        # print(h_in_deriv.shape, self.delta_j.shape)
 
     def _weight_updating(self, index):
         self.w = self.w + self.learning_rate * \
             self.delta_k[:, index] * self.h[:, index]
         self.v = self.v + self.learning_rate * \
-            np.dot(np.array([self.delta_j[:, index]]).T,
+            np.dot(np.array([self.delta_j[0:-1, index]]).T,
                    np.array([self.data[index, :]]))
-
-        # print(self.v)
-        # self.w = self.w + self.learning_rate * np.dot()
 
     def fit(self, data, labels, n_epochs):
         '''
@@ -90,8 +82,7 @@ class NeuralNetwork():
         self.miss_ratio = np.zeros(n_epochs+1)
         self.misses = np.zeros(n_epochs+1)
         self._add_bias()
-        # self._forward_pass()
-        # self._backward_pass(labels)
+
         for i in range(n_epochs):
             if self.method == 'sequential':
                 for index in range(self.n_data):
@@ -111,7 +102,9 @@ class NeuralNetwork():
                 np.count_nonzero(self.result == self.labels)
 
     def predict(self, data_point):
+        data_point = np.concatenate((data_point, np.ones(1)))
         h = self._activation(np.dot(self.v, data_point))
+        h = np.concatenate((h, np.ones(1)))  # add bias
         y = self._activation(np.dot(self.w, h))
         return y
 
@@ -144,10 +137,9 @@ def plot_decision_boundry(data, res, predictor):
     y_range = np.linspace(y_min, y_max, res)
     result = np.ones((res, res))
     y_out = np.zeros(res)
-
     for row in range(res):
         for col in range(res):
-            result[row, col] = np.abs(predictor([x_out[col], y_range[row], 1]))
+            result[row, col] = np.abs(predictor([x_out[col], y_range[row]]))
 
     for i in range(res):
         y_out[i] = y_range[np.argmin(result[:, i])]
@@ -155,32 +147,20 @@ def plot_decision_boundry(data, res, predictor):
     plt.plot(x_out, y_out)
 
 
-def test_num_nodes():
-    test = [1, 5, 10, 15, 20, 30]
-    N = 100
-    for i in test:
-        plt.subplot(1,1,2)
-        plt.plot()
-def test_network():
-    N = 100
-    network = NeuralNetwork(method='batch', n_inputs=2,
-                            n_hidden=3, n_outputs=1)
-                            # n_hidden
+N = 100
+network = NeuralNetwork(method='batch', n_inputs=2,
+                        n_hidden=1, n_outputs=1)
+# n_hidden = 30
 
-    # data = generate_data(N, plot=True, meanA=[0, 0], meanB=[
-    #     10, 10], sigmaA=2, sigmaB=2)
-    # data = generate_data(N, plot=True, meanA=[2, 1.5], meanB=[
-    # 0, -1], sigmaA=0.2, sigmaB=0.3)
+# data = generate_data(N, plot=True, meanA=[0, 0], meanB=[
+#     10, 10], sigmaA=2, sigmaB=2)
+data = generate_data(N, plot=True, meanA=[2, 1.5], meanB=[
+    0, -1], sigmaA=0.2, sigmaB=0.3)
+# data = generate_data(N, plot=True, meanA=[1, 0.5], meanB=[
+#     0, -1], sigmaA=0.2, sigmaB=0.3)
 
-    np.random.seed(2)
-    data = generate_data(N, plot=True, meanA=[2, 2.5], meanB=[
-        0, 0], sigmaA=0.8, sigmaB=0.5)
-    
-    network.fit(data[:, 0:2], data[:, 2], n_epochs=100)
-    mse, miss_ratio = network.metrics()
-    print(mse[-1], miss_ratio[-1])
-    plot_decision_boundry(data, 500, network.predict)
-    plt.show()
-
-test_network()
-# test_num_nodes()
+network.fit(data[:, 0:2], data[:, 2], n_epochs=100)
+mse, miss_ratio = network.metrics()
+print(mse[-1], miss_ratio[-1])
+plot_decision_boundry(data, 500, network.predict)
+plt.show()
