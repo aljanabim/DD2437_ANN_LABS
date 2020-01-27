44e5e0ac18fa1dafadcebbbe9fad72c422f49b56
Mustafa Al-Janabi <ma970101@yahoo.se>
2020-01-27 13:55:05 +0100

    valid added

--- lab1/generate_data.py
+++ lab1/generate_data.py
@@ -42,3 +42,50 @@ def generate_data(N, plot=False, meanA=None, meanB=None, sigmaA=None, sigmaB=Non
 
         plt.plot()
     return data
+
+
+def generate_data_2(N, plot=False, meanA=None, meanB=None, sigmaA=None, sigmaB=None, special=False):
+    '''
+    Generates data of two linearly seperable classes of N samples
+    '''
+    # Set up gaussian distribution parameters
+    if not meanA:
+        meanA = [4, 2]
+    if not sigmaA:
+        covA = np.array([[0.5, 0],
+                         [0, 0.5]])
+    else:
+        covA = np.array([[sigmaA, 0],
+                         [0, sigmaA]])
+    if not meanB:
+        meanB = [-2, 2]
+    if not sigmaB:
+        covB = np.array([[0.5, 0],
+                         [0, 0.5]])
+    else:
+        covB = np.array([[sigmaB, 0],
+                         [0, sigmaB]])
+
+    classA = np.random.multivariate_normal(meanA, covA**2, N)
+    classB = np.random.multivariate_normal(meanB, covB**2, N)
+
+    if special:
+        meanA_alternative = meanA.copy()
+        meanA_alternative[0] *= -1
+        classA_alternative = np.random.multivariate_normal(
+            meanA_alternative, covA**2, N)
+        classA[:int(N/2), :] = classA_alternative[:int(N/2), :]
+
+    classA_extended = np.column_stack([classA, np.ones(N)])
+    classB_extended = np.column_stack([classB, -np.ones(N)])
+    data = np.row_stack([classA_extended, classB_extended])
+    np.random.shuffle(data)
+    # data = np.array([[classA, np.zeros(N)],
+    #                  [classB, np.ones(N)]])
+    # print(data)
+    if plot:
+        plt.scatter(classA[:, 0], classA[:, 1], label="Class A")
+        plt.scatter(classB[:, 0], classB[:, 1], label="Class B")
+        # plt.show()
+
+    return data
