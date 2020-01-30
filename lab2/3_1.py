33fcc460ccba8f448af906bcf87aa2d3c22a136e
Mustafa Al-Janabi <ma970101@yahoo.se>
2020-01-30 14:19:02 +0100

    message

--- lab2/3_1.py
+++ lab2/3_1.py
@@ -6,20 +6,22 @@ VAR = 0.1
 
 
 class RBFNetwork():
-    def __init__(self, n_inputs, n_rbf, n_outputs, min_val=0.05, max_val=2*np.pi):
+    def __init__(self, n_inputs, n_rbf, n_outputs, learning_rate=0.1, min_val=0.05, max_val=2*np.pi):
         self.n_inputs = n_inputs
         self.n_rbf = n_rbf
         self.n_outputs = n_outputs
         self.rbf_centers = np.linspace(min_val, max_val, n_rbf)
-        self.w = np.random.normal(0, 1, n_rbf)
+        self.w = np.array([np.random.normal(0, 1, n_rbf)])
 
     def RBF(self, x, center):
         return np.exp(-np.linalg.norm(x-center)**2/(2*VAR**2))
 
-    def fit(self, data, method='batch'):
-        self.data = data
+    def fit(self, data, f, method='batch'):
+        self.data = np.array([data]).T
         if method == 'batch':
-            phi =
+            phi = np.dot(self.data, )
+            w = np.linalg.pinv(np.dot(phi.T, phi)), np.
+            print(w)
 
 
 def sin2(x):
@@ -46,5 +48,6 @@ square_train = list(map(square, generate_input(0)))
 square_test = list(map(square, generate_input(0.05)))
 
 network = RBFNetwork(n_inputs=1, n_rbf=4, n_outputs=1)
-network.RBF(0.5, 0.45)
-print(network.w)
+network.fit(sin2_train, sin2_train)
+# network.RBF(0.5, 0.45)
+# print(network.w)
