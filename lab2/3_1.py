2429d40eb7f2bb9b117019ac5b60105063006cf7
lennelov <lennelov@kth.se>
2020-01-30 16:25:18 +0100

    Nothing

--- lab2/3_1.py
+++ lab2/3_1.py
@@ -25,6 +25,7 @@ class RBFNetwork():
             self.w = np.dot(
                 np.dot(np.linalg.pinv(np.dot(phi.T, phi)), phi.T), f)
 
+
     def predict(self, x):
         x = np.array([x]).T
         return np.dot(self.w, self.RBF(x, self.rbf_centers).T)
@@ -51,7 +52,6 @@ def square(x):
         return -1
 
 
-<<<<<<< HEAD
 def generate_input(start, n_points=10):
     patterns = np.linspace(start, 2*np.pi, n_points)
     np.random.shuffle(patterns)
@@ -61,18 +61,6 @@ def generate_input(start, n_points=10):
 def gen_func_data(n_train, n_test, func):
     patterns = np.linspace(0, 2*np.pi, n_train+n_test)
     targets = np.array([func(x) for x in patterns])
-=======
-def generate_input(start, end=2*np.pi):
-    patterns = np.arange(start, end, 0.1)
-    # np.random.shuffle(patterns)
-    return patterns
-
-
-def gen_sin_data():
-    sin2_train = [sin2(val) for val in generate_input(0)]
-    sin2_test = [sin2(val) for val in generate_input(0.05)]
-    return sin2_train, sin2_test
->>>>>>> ac6807c0d69b524f2a726cf5dcbb94a02a3698ae
 
     data = np.column_stack((patterns, targets))
     np.random.shuffle(data)
@@ -89,7 +77,6 @@ def gen_sin_data():
 
 
 def plot_prediction():
-<<<<<<< HEAD
     n_train = 64
     n_test = 63
 
@@ -103,29 +90,6 @@ def plot_prediction():
 
 
 plot_prediction()
-<<<<<<< HEAD
-=======
-=======
-    network = RBFNetwork(n_inputs=1, n_rbf=63, n_outputs=1)
-    sin2_train, sin2_test = gen_sin_data()
-    square_train, square_test = gen_square_data()
-    network.fit(square_train, square_train)
-
-    x = np.linspace(0, 2*np.pi, 100)
-    y = list(map(network.predict, x))
-    sin2_target = list(map(sin2, x))
-    square_target = list(map(square, x))
-
-    plt.plot(x, y, label='Fit')
-    plt.plot(x, sin2_target, label='Target')
-    plt.legend()
-    plt.show()
-
-
-def plot_error():
-    # plot error as function of number of rbfs
-    rbf_ = np.arange(0, 63)
->>>>>>> ac6807c0d69b524f2a726cf5dcbb94a02a3698ae
 
 
 x_train = generate_input(0)
@@ -136,10 +100,9 @@ sin2_test = list(map(sin2, generate_input(0.05)))
 square_train = list(map(square, generate_input(0)))
 square_test = list(map(square, generate_input(0.05)))
 
-network = RBFNetwork(n_inputs=1, n_rbf=62, n_outputs=1)
-network.fit(x_train, sin2_train)
+network = RBFNetwork(n_inputs=1, n_rbf=63, n_outputs=1)
+network.fit(sin2_train, sin2_train)
 print(network.predict([0.5, 0]))
 print(sin2(np.array([0.5, 0])))
 # network.RBF(0.5, 0.45)
 # print(network.w)
->>>>>>> 9bc39c6292520f816afb9124526702169c017220
