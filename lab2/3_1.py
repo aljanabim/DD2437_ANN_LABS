bc6c3061ad306e7099d9a8d2545dd850f3e47060
lennelov <lennelov@kth.se>
2020-01-30 15:44:41 +0100

    Update

--- lab2/3_1.py
+++ lab2/3_1.py
@@ -65,17 +65,18 @@ def gen_sin_data():
     sin2_test = [sin2(val) for val in generate_input(0.05)]
     return sin2_train, sin2_test
 
+
 def gen_square_data():
-    square_train =  [square(val) for val in generate_input(0)]
-    square_test =  [square(val) for val in generate_input(0.05)]
+    square_train = [square(val) for val in generate_input(0)]
+    square_test = [square(val) for val in generate_input(0.05)]
     return square_train, square_test
 
 
 def plot_prediction():
     network = RBFNetwork(n_inputs=1, n_rbf=50, n_outputs=1)
     sin2_train, sin2_test = gen_sin_data()
-    square_train, square_test = gen_square()
-    network.fit(sin2_train, sin2_test)
+    square_train, square_test = gen_square_data()
+    network.fit(square_train, square_test)
 
     x = np.linspace(0, 2*np.pi, 100)
     y = np.zeros(x.shape)
