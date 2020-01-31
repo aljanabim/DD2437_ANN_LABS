f4f0810f553c33daec5a5b285cf702bc5d405f05
lennelov <lennelov@kth.se>
2020-01-31 12:38:18 +0100

    Make modular

--- lab2/3_1.py
+++ lab2/3_1.py
@@ -1,40 +1,11 @@
 # %%
 import numpy as np
 from matplotlib import pyplot as plt
+from rbf_net import RBFNetwork
 plt.style.use('ggplot')
 VAR = 0.1
 
 np.random.seed(3)
-
-
-class RBFNetwork():
-    def __init__(self,
-                 n_inputs,
-                 n_rbf,
-                 n_outputs,
-                 learning_rate=0.1,
-                 min_val=0.05,
-                 max_val=2 * np.pi):
-        self.n_inputs = n_inputs
-        self.n_rbf = n_rbf
-        self.n_outputs = n_outputs
-        self.rbf_centers = np.array([np.linspace(min_val, max_val, n_rbf)])
-        self.w = np.array([np.random.normal(0, 1, n_rbf)])
-        self.RBF = np.vectorize(self._base_func)
-
-    def _base_func(self, x, center):
-        return np.exp(-np.linalg.norm(x - center)**2 / (2 * VAR**2))
-
-    def fit(self, data, f, method='batch'):
-        self.data = np.array([data]).T
-        if method == 'batch':
-            phi = self.RBF(self.data, self.rbf_centers)
-            self.w = np.dot(np.dot(np.linalg.pinv(np.dot(phi.T, phi)), phi.T),
-                            f)
-
-    def predict(self, x):
-        x = np.array([x]).T
-        return np.dot(self.w, self.RBF(x, self.rbf_centers).T)
 
 
 def plot_prediction():
@@ -93,13 +64,14 @@ def plot_prediction(func):
     n_train = 64
     n_test = 63
 
-    network = RBFNetwork(n_inputs=1, n_rbf=10, n_outputs=1)
+    network = RBFNetwork(n_inputs=1, n_rbf=100, n_outputs=1)
     train_patterns, train_targets, test_patterns, test_targets = gen_func_data(
         n_train, n_test, func)
 
-    network.fit(train_patterns, train_targets)
+    network.fit(train_patterns, train_targets, method='sequential')
     train_preds = network.predict(train_patterns)
     targets = list(map(func, train_patterns))
+    print(train_preds)
     plt.plot(train_patterns, train_preds, 'o', color='m', label='Estimated')
     plt.plot(train_patterns, targets, '+', color='c', label='Target')
     plt.legend()
@@ -123,7 +95,7 @@ def plot_error(func, MAKE_SQAURE_GREAT=False):
     error_test_improved = np.zeros(len(rbfs))
 
     for i, n in enumerate(rbfs):
-        network = RBFNetwork(n_inputs=1, n_rbf=n, n_outputs=1)
+        network = RBFNetwork(n_inputs=1, n_rbf=n, n_outputs=1, n_epochs=10)
         network.fit(train_patterns, train_targets)
 
         train_preds_imporved = list(
@@ -157,10 +129,12 @@ def plot_error(func, MAKE_SQAURE_GREAT=False):
     plt.legend()
     plt.show()
 
+#
+# plot_error(func=sin2, MAKE_SQAURE_GREAT=False)
+# plot_error(func=square, MAKE_SQAURE_GREAT=False)
+# plot_error(func=square, MAKE_SQAURE_GREAT=True)
 
-plot_error(func=sin2, MAKE_SQAURE_GREAT=False)
-plot_error(func=square, MAKE_SQAURE_GREAT=False)
-plot_error(func=square, MAKE_SQAURE_GREAT=True)
+plot_prediction(func=sin2)
 
 
 # x_train = generate_input(0)
---
+++ lab2/rbf_net.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+class RBFNetwork():
+    def __init__(self,
+                 n_inputs,
+                 n_rbf,
+                 n_outputs,
+                 n_epochs = 100,
+                 learning_rate=0.1,
+                 min_val=0.05,
+                 max_val=2 * np.pi,
+                 rbf_var=0.1):
+        self.n_inputs = n_inputs
+        self.n_rbf = n_rbf
+        self.n_outputs = n_outputs
+        self.n_epochs = n_epochs
+        self.learning_rate = learning_rate
+        self.rbf_var = rbf_var
+
+        self.rbf_centers = np.array([np.linspace(min_val, max_val, n_rbf)])
+        self.w = np.array([np.random.normal(0, 1, n_rbf)])
+        self.RBF = np.vectorize(self._base_func)
+
+    def _base_func(self, x, center):
+        return np.exp(-np.linalg.norm(x - center)**2 / (2 * self.rbf_var**2))
+
+    def fit(self, data, f, method='batch'):
+        self.data = np.array([data]).T
+
+        if method == 'batch':
+            phi = self.RBF(self.data, self.rbf_centers)
+            self.w = np.dot(np.dot(np.linalg.pinv(np.dot(phi.T, phi)), phi.T), f)
+        if method == "sequential":
+            for _ in range(self.n_epochs):
+                for k, x_k in enumerate(data):
+                    error = f[k] - self.predict(x_k)
+                    delta_w = self.learning_rate * error * self.RBF(x_k, self.rbf_centers)
+                    self.w += delta_w
+
+    def predict(self, x):
+        x = np.array([x]).T
+        return np.reshape(np.dot(self.w, self.RBF(x, self.rbf_centers).T), (-1))
