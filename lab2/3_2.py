2d5e26a8a2bdf725f8461a51753f1797fb6773d4
lennelov <lennelov@kth.se>
2020-01-31 15:36:50 +0100

    Add learning rate changer (patent pending)

--- lab2/3_2.py
+++ lab2/3_2.py
@@ -5,6 +5,16 @@ plt.style.use('ggplot')
 
 np.random.seed(3)
 
+def sin2(x):
+    return np.sin(2 * x)
+
+
+def square(x):
+    if np.sin(2 * x) >= 0:
+        return 1
+    else:
+        return -1
+
 
 def gen_func_data(n_train, n_test, func, noise_var=0):
     patterns = np.linspace(0, 2 * np.pi, n_train + n_test)
@@ -28,28 +38,29 @@ def gen_func_data(n_train, n_test, func, noise_var=0):
 def plot_prediction(func):
     n_train = 64
     n_test = 63
+    func = sin2
 
-    network = RBFNetwork(n_inputs=1, n_rbf=60, n_outputs=1, n_epochs=200)
+    network = RBFNetwork(n_inputs=1, n_rbf=64, n_outputs=1, n_epochs=200,
+                         learning_rate_start=0.1, learning_rate_end=0.001)
     train_patterns, train_targets, test_patterns, test_targets = gen_func_data(
-        n_train, n_test, func, noise_var=0.1)
+        n_train, n_test, func, noise_var=0.001)
 
-    network.fit(train_patterns, train_targets, method='batch')
+    print(network.learning_rate)
+    print(network.learning_rate_decay)
+    print(np.exp(-network.learning_rate_decay))
+    network.fit(train_patterns, train_targets, method='sequential')
     train_preds = network.predict(train_patterns)
+    print(network.learning_rate)
     plt.plot(train_patterns, train_preds, 'o', color='m', label='Estimated')
     plt.plot(train_patterns, train_targets, '+', color='c', label='Target')
     plt.legend()
     plt.show()
 
+    plt.plot(network.learning_rate_record)
+    plt.show()
 
-def sin2(x):
-    return np.sin(2 * x)
 
 
-def square(x):
-    if np.sin(2 * x) >= 0:
-        return 1
-    else:
-        return -1
 
 
 plot_prediction(square)
--- lab2/rbf_net.py
+++ lab2/rbf_net.py
@@ -7,7 +7,8 @@ class RBFNetwork():
                  n_rbf,
                  n_outputs,
                  n_epochs=100,
-                 learning_rate=0.1,
+                 learning_rate_start=0.1,
+                 learning_rate_end=0.001,
                  min_val=0.05,
                  max_val=2 * np.pi,
                  rbf_var=0.1):
@@ -15,12 +16,14 @@ class RBFNetwork():
         self.n_rbf = n_rbf
         self.n_outputs = n_outputs
         self.n_epochs = n_epochs
-        self.learning_rate = learning_rate
         self.rbf_var = rbf_var
 
         self.rbf_centers = np.array([np.linspace(min_val, max_val, n_rbf)])
         self.w = np.array([np.random.normal(0, 1, n_rbf)])
         self.RBF = np.vectorize(self._base_func)
+        self.learning_rate_decay = -(1/n_epochs) * ( np.log(learning_rate_end) - np.log(learning_rate_start) )
+        self.learning_rate = learning_rate_start
+        self.learning_rate_record = []
 
     def _base_func(self, x, center):
         return np.exp(-np.linalg.norm(x - center)**2 / (2 * self.rbf_var**2))
@@ -39,6 +42,8 @@ class RBFNetwork():
                     delta_w = self.learning_rate * error * \
                         self.RBF(x_k, self.rbf_centers)
                     self.w += delta_w
+                self.learning_rate *= np.exp(-self.learning_rate_decay)
+                self.learning_rate_record.append(self.learning_rate)
 
     def predict(self, x):
         x = np.array([x]).T
