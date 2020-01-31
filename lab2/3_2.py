06690c2d253af92c33f1b6cc2e92d444b967dc35
lennelov <lennelov@kth.se>
2020-01-31 12:38:39 +0100

    Add

--- lab2/3_2.py
+++ lab2/3_2.py
@@ -1,37 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from rbf_net import RBFNetwork
 plt.style.use('ggplot')
-VAR = 0.1
 
 np.random.seed(3)
-
-
-class RBFNetwork():
-    def __init__(self,
-                 n_inputs,
-                 n_rbf,
-                 n_outputs,
-                 learning_rate=0.1,
-                 min_val=0.05,
-                 max_val=2 * np.pi):
-        self.n_inputs = n_inputs
-        self.n_rbf = n_rbf
-        self.n_outputs = n_outputs
-        self.rbf_centers = np.array([np.linspace(min_val, max_val, n_rbf)])
-        self.w = np.array([np.random.normal(0, 1, n_rbf)])
-        self.RBF = np.vectorize(self._base_func)
-
-    def _base_func(self, x, center):
-        return np.exp(-np.linalg.norm(x - center)**2 / (2 * VAR**2))
-
-    def fit(self, data, f, method='batch'):
-        self.data = np.array([data]).T
-        if method == 'batch':
-            phi = self.RBF(self.data, self.rbf_centers)
-            self.w = np.dot(np.dot(np.linalg.pinv(np.dot(phi.T, phi)), phi.T),
-                            f)
-        if method == "sequential":
-
-    def predict(self, x):
-        x = np.array([x]).T
-        return np.dot(self.w, self.RBF(x, self.rbf_centers).T)
