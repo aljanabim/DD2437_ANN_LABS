2c2cb7ea1204a52bc7bf569577f4dfd85d2f567d
lennelov <lennelov@kth.se>
2020-02-04 13:31:52 +0100

    Add 3.3 stuff

--- lab2/rbf_net.py
+++ lab2/rbf_net.py
@@ -9,6 +9,8 @@ class RBFNetwork():
                  n_epochs=100,
                  learning_rate_start=0.1,
                  learning_rate_end=0.001,
+                 cl_learning_rate = 0.2,
+                 cl_leak_rate = 0.001,
                  min_val=0.05,
                  max_val=2 * np.pi,
                  rbf_var=0.1):
@@ -23,31 +25,49 @@ class RBFNetwork():
         self.RBF = np.vectorize(self._base_func)
         self.learning_rate_decay = -(1/n_epochs) * ( np.log(learning_rate_end) - np.log(learning_rate_start) )
         self.learning_rate = learning_rate_start
+        self.cl_learning_rate = cl_learning_rate
+        self.cl_leak_rate = cl_leak_rate
+
         self.learning_rate_record = []
         self.mse_record = None
 
     def _base_func(self, x, center):
         return np.exp(-np.linalg.norm(x - center)**2 / (2 * self.rbf_var**2))
 
-    def fit(self, data, f, method='batch'):
+    def fit(self, data, f, method='batch', cl_method=None):
         self.mse_record = []
         self.data = np.array([data]).T
         if method == 'batch':
             phi = self.RBF(self.data, self.rbf_centers)
             self.w = np.dot(
                 np.dot(np.linalg.pinv(np.dot(phi.T, phi)), phi.T), f)
-        if method == "sequential":
+        elif method == "sequential":
             for _ in range(self.n_epochs):
                 for k, x_k in enumerate(data):
-                    error = f[k] - self.predict(x_k)
-                    delta_w = self.learning_rate * error * \
-                        self.RBF(x_k, self.rbf_centers)
-                    self.w += delta_w
+                    if cl_method == "basic":
+                        self._cl_step(x_k, leaky=False)
+                    elif cl_method == 'leaky':
+                        self._cl_step(x_k, leaky=True)
+                    self.w += self._calc_delta_w(x_k, f[k])
+
                 self.learning_rate *= np.exp(-self.learning_rate_decay)
                 self.learning_rate_record.append(self.learning_rate)
-
                 preds = self.predict(data)
                 self.mse_record.append(self.calc_mse(preds, f))
+
+    def _calc_delta_w(self, pattern, target):
+        error = target - self.predict(pattern)
+        delta_w = self.learning_rate * error * \
+            self.RBF(pattern, self.rbf_centers)
+        return delta_w
+
+    def _cl_step(self, pattern, leaky=False):
+        rbf_values = self.RBF(pattern, self.rbf_centers)
+        winner_index = np.argmax(rbf_values)
+        winner_center = self.rbf_centers[0, winner_index]
+        self.rbf_centers[0, winner_index] += self.cl_learning_rate*(pattern - winner_center)
+        if leaky:
+            self.rbf_centers[0, :] += self.cl_leak_rate*(pattern - self.rbf_centers[0, :])
 
     def calc_mse(self, preds, targets):
         return np.sum(np.power(preds - targets, 2))/len(targets)
