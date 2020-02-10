80e8ab302e006eefcb084c665e44a206b43b45d5
Lennelov <einarlennelov@gmail.com@git config --global user.email einarlennelov@gmail.comgit config --global user.email einarlennelov@gmail.com@>
2020-02-10 22:11:25 +0100

    Add show image function

--- lab3/3_2.py
+++ lab3/3_2.py
@@ -1,14 +1,30 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
+from hopfield import HopfieldNet
 
 
 def load_data():
     with  open('lab3/data/pict.dat', 'r') as f:
-        text = str(f.read()).split(',')
-        value_list = np.array([int(val) for val in text])
-        patterns = []
+        text = str(f.read())
+        value_list = np.array([int(val) for val in text.split(',')])
+        images = []
         for n in range(11):
             start_index = 1024*n
             end_index = 1024*(n+1)
-            patterns.append(value_list[start_index:end_index])
-        return np.array(patterns)
+            images.append(value_list[start_index:end_index])
+        return np.array(images)
+
+
+def show_image(image):
+    image = np.reshape(image, (32, 32))
+    plt.imshow(image)
+    plt.show()
+
+
+def main():
+    images = load_data()
+    show_image(images[9])
+
+
+if __name__ == '__main__':
+    main()
