be4d0363b38577e17a151d32a846ed6511133c2d
Lennelov <einarlennelov@gmail.com@git config --global user.email einarlennelov@gmail.comgit config --global user.email einarlennelov@gmail.com@>
2020-02-10 18:32:52 +0100

    Add skeleton functions

--- lab3/3_1.py
+++ lab3/3_1.py
@@ -1,6 +1,7 @@
 import numpy as np
 from hopfield import HopfieldNet
 
+# ======= DATA HANDLING ===========
 
 def get_clean_data():
     patterns = np.array([[-1, -1,  1, -1,  1, -1, -1,  1],
@@ -16,7 +17,13 @@ def get_noisy_data():
     return patterns
 
 
+# ======= TESTING AND PLOTTING ===========
+
+
 def test_noise_reduction():
+    """Apply the update rule repeatedly until you reach a stable xed point. Did
+       all the patterns converge towards stored patterns?"""
+
     clean_patterns = get_clean_data()
     noisy_patterns = get_noisy_data()
 
@@ -25,6 +32,22 @@ def test_noise_reduction():
     noisy_preds = net.predict(noisy_patterns)
 
     print(noisy_preds == clean_patterns)
+
+
+def find_attractors():
+    """How many attractors are there in this network?
+       Hint: automate the searching."""
+
+    # TODO: Implement
+    pass
+
+
+def test_more_noisy():
+    """What happens when you make the starting pattern even more dissimilar
+       to the stored ones (e.g. more than half is wrong)?"""
+       
+    # TODO: Implement
+    pass
 
 
 
