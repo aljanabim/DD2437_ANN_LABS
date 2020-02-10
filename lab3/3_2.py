30d24f7e23506a13ebdd7734e115acbabdda975c
Lennelov <einarlennelov@gmail.com@git config --global user.email einarlennelov@gmail.comgit config --global user.email einarlennelov@gmail.com@>
2020-02-10 22:41:01 +0100

    Add test image recovery function

--- lab3/3_2.py
+++ lab3/3_2.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from hopfield import HopfieldNet
 
+# ============= HELPER FUNCTIONS ===============
 
 def load_data():
     with  open('lab3/data/pict.dat', 'r') as f:
@@ -16,15 +17,78 @@ def load_data():
 
 
 def show_image(image):
-    image = np.reshape(image, (32, 32))
-    plt.imshow(image)
+        image = np.reshape(image, (32, 32)).T
+        plt.imshow(image)
+
+
+def calc_element_accuracy(patterns, preds):
+    if len(patterns.shape) == 1:
+        patterns = np.reshape(patterns, (-1, 1))
+
+    n_total = patterns.shape[0] * patterns.shape[1]
+    n_correct = np.sum(patterns == preds)
+    return n_correct / n_total
+
+
+def calc_sample_accuracy(patterns, preds):
+    n_total = patterns.shape[0]
+    n_correct = 0
+    for pattern, pred in zip(patterns, preds):
+        if (pattern == pred).all():
+            n_correct += 1
+    return n_correct / n_total
+
+
+# ============= TESTING AND PLOTTING ===============
+
+
+def stability_check():
+    """Check that the three patterns are stable."""
+    images = load_data()
+    base_images = images[:3]
+
+    net = HopfieldNet(max_iter=100)
+    net.fit(base_images)
+    preds = net.predict(base_images)
+    accuracy = calc_element_accuracy(base_images, preds)
+    print("Accuracy on training data: ".format(accuracy))
+
+
+def test_image_recovery():
+    images = load_data()
+    base_image = images[0]
+    degraded_image = images[9]
+
+    net = HopfieldNet(max_iter=1)
+    net.fit(base_image)
+    recovered_image = net.predict(degraded_image)
+    accuracy = calc_element_accuracy(base_image, recovered_image)
+
+    plt.subplot(131)
+    show_image(base_image)
+    plt.subplot(132)
+    show_image(degraded_image)
+    plt.subplot(133)
+    show_image(recovered_image)
     plt.show()
 
+    print("Accuracy on degraded data: ".format(accuracy))
 
-def main():
+
+
+
+
+
+
+
+
+
+
+def test_show_image():
     images = load_data()
-    show_image(images[9])
+    show_image(images[10])
 
 
 if __name__ == '__main__':
-    main()
+    stability_check()
+    test_image_recovery()
