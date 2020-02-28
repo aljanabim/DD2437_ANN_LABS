83db9c7944dfb8197319f4cf150c8cb790d396df
Mustafa Al-Janabi <ma970101@yahoo.se>
2020-02-28 15:23:15 +0100

    blabla

--- lab4/4_2.py
+++ lab4/4_2.py
@@ -2,8 +2,6 @@ from util import *
 from rbm import RestrictedBoltzmannMachine
 from dbn import DeepBeliefNet
 import time
-import matplotlib.pyplot as plt
-plt.style.use('ggplot')
 
 '''
 Results:
@@ -20,64 +18,27 @@ if __name__ == "__main__":
 
     image_size = [28, 28]
     train_imgs, train_lbls, test_imgs, test_lbls = read_mnist(
-<<<<<<< HEAD
         dim=image_size, n_train=6000, n_test=1000)
-=======
-        dim=image_size, n_train=60000, n_test=10000)
-
-    print(train_lbls)
->>>>>>> 5e1b5d05fd9c9bd31dac366558aca12b6ee01134
 
     ''' deep- belief net '''
 
     print("\nStarting a Deep Belief Net..")
 
-
+    dbn = DeepBeliefNet(sizes={"vis": image_size[0]*image_size[1], "hid": 500, "pen": 500, "top": 2000, "lbl": 10},
+                        image_size=image_size,
+                        n_labels=10,
+                        batch_size=15)
 
     ''' greedy layer-wise training '''
-    train_accuracy_list = []
-    test_accuracy_list = []
-    # iter_values = [20, 100, 400, 800, 1200]
-    iter_values = [2000]
-    for n_iter in iter_values:
-        dbn = DeepBeliefNet(sizes={"vis": image_size[0]*image_size[1], "hid": 500, "pen": 500, "top": 2000, "lbl": 10},
-                            image_size=image_size,
-                            n_labels=10,
-                            batch_size=15)
+    train_start_time = time.time()
+    dbn.train_greedylayerwise(vis_trainset=train_imgs,
+                              lbl_trainset=train_lbls, n_iterations=800)
+    train_end_time = time.time()
+    print("Train time: {}s".format(train_end_time - train_start_time))
 
-        train_start_time = time.time()
-        dbn.train_greedylayerwise(vis_trainset=train_imgs,
-                                  lbl_trainset=train_lbls, n_iterations=n_iter, make_plots=False)
-        train_end_time = time.time()
-        print("Train time: {}s".format(train_end_time - train_start_time))
+    dbn.recognize(train_imgs, train_lbls)
 
-        train_accuracy = dbn.recognize(train_imgs, train_lbls)
-        test_accuracy = dbn.recognize(test_imgs, test_lbls)
-
-        train_accuracy_list.append(train_accuracy)
-        test_accuracy_list.append(test_accuracy)
-
-    print(train_accuracy_list)
-    print(test_accuracy_list)
-    plt.plot(iter_values, train_accuracy_list, label="Train accuracy")
-    plt.plot(iter_values, test_accuracy_list, label="Test accuracy")
-    plt.xlabel("Number of iterations")
-    plt.ylabel("Accuracy")
-    plt.legend()
-    plt.show()
-
-<<<<<<< HEAD
-
-    generate_start_time = time.time()
-    for digit in range(10):
-        digit_1hot = np.zeros(shape=(1, 10))
-        digit_1hot[0, digit] = 1
-        dbn.generate(digit_1hot, name="rbms")
-    generate_end_time = time.time()
-    print("Generate time: {}s".format(generate_end_time - generate_start_time))
-=======
     dbn.recognize(test_imgs, test_lbls)
-<<<<<<< HEAD
 
     generate_start_time = time.time()
     all_last = []
@@ -98,14 +59,3 @@ if __name__ == "__main__":
     plt.savefig('plots/gen_4_2_samples6000iter800.pdf')
     generate_end_time = time.time()
     print("Generate time: {}s".format(generate_end_time - generate_start_time))
-=======
-    # 
-    # generate_start_time = time.time()
-    # for digit in range(10):
-    #     digit_1hot = np.zeros(shape=(1, 10))
-    #     digit_1hot[0, digit] = 1
-    #     dbn.generate(digit_1hot, name="rbms")
-    # generate_end_time = time.time()
-    # print("Generate time: {}s".format(generate_end_time - generate_start_time))
->>>>>>> 7eff1a4841f8c7bb4a59533e7c036d8b6d867782
->>>>>>> 5e1b5d05fd9c9bd31dac366558aca12b6ee01134
